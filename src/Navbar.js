10a0eecef7a836fe6d88b4a26f7760c08be84583
Soladragon <soladragon@pop-os.localdomain>
2022-12-07 00:01:39 +0000

    removed some imports

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Routes, Route, useLocation, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NavbarFixed = () => (
 
