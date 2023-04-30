875549583756702f880b6d0ee104dc3ce0252907
John <soladragon@debian-BULLSEYE-live-builder-AMD64>
2023-04-30 19:28:20 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,43 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, useLocation, Link } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import logo from './logo.svg';
 import './App.css';
 import Github from './Github';
-import { Routes, Route, useLocation, Link } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import NavbarFixed from './Navbar';
-import { Container } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
 import PortfolioCard from './PortfolioCard';
-import { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Cover.css';
-import { Nav } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-
 
 //https://react-bootstrap.github.io/getting-started/introduction
 //https://www.freecodecamp.org/news/how-to-use-react-router-version-6/#:~:text=To%20install%20React%20Router%2C%20all,%2Drouter%2Ddom%406%20.
 
 function App() {
-
   const location = useLocation();
-
   const [data, setData] = useState([]);
-
   const [open, setOpen] = useState(false);
-
-
-
-
 
   const fetchData = async () => {
     try {
-
       //https://api.github.com/repos/soladragon/portfolio/languages
-
       //alternative https://api.github.com/search/repositories?q=user:soladragon&fork:false&direction=desc
       const response = await fetch('https://api.github.com/users/soladragon/repos?sort=updated&direction=desc&fork=false');
-
       const json = await response.json();
       setData(json);
     } catch (error) {
@@ -46,56 +32,37 @@ function App() {
   }
 
   useEffect(() => {
-    setTimeout(function () {
-
+    setTimeout(() => {
       // code to be executed after 0 seconds
       fetchData()
-
     }, 0);
-
   }, []);
-
   return (
     <div className="App h-100 text-center text-bg-dark">
-
       <Navbar bg="dark" expand="lg" className="mx-auto" variant="dark" fixed="top" >
-
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="mx-auto" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto" style={{ marginTop: '20px' }}>
             <Link className="nav-link fw-bold active" to="/">Home</Link>
-            <Link className="nav-link fw-bold " to="/github">Github</Link>
-            <Link className="nav-link fw-bold " to="/github">Contact Me</Link>
-
+            <Link className="nav-link fw-bold" to="/github">Github</Link>
+            <Link className="nav-link fw-bold" to="/github">Contact Me</Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
       <TransitionGroup>
         <CSSTransition key={location.key} classNames="slide" timeout={300}>
-
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={
-              <div className='vertical-center '>
-                <main class="px-3 ">
+              <div className='vertical-center'>
+                <main className="px-3">
                   <h1>Hello, I am John Murphy</h1>
-                  <p class="lead">Experienced Software Developer with a proven track record of delivering high-quality software solutions.
-                    Skilled in various programming languages and technologies, with expertise in C#, ASP.NET, and React. Dedicated to staying current with industry best practices.</p>
-
-
+                  <p className="lead">Experienced Software Developer with a proven track record of delivering high-quality software solutions. Skilled in various programming languages and technologies, with expertise in C#, ASP.NET, and React. Dedicated to staying current with industry best practices.</p>
                 </main>
-
               </div>
-
-            }
-            />
-            <Route path="github" element={
-
-              <Github></Github>
-
             } />
+            <Route path="github" element={<Github />} />
           </Routes>
-
         </CSSTransition>
       </TransitionGroup>
     </div >
--- src/PortfolioCard.js
+++ src/PortfolioCard.js
@@ -1,33 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
 import Collapse from 'react-bootstrap/Collapse';
-import { useEffect } from 'react';
 import Fade from 'react-bootstrap/Fade';
 
-
-
+import logo from './logo.svg';
+import './App.css';
 
 const PortfolioCard = ({ data, index }) => {
-
   //https://api.github.com/repos/soladragon/portfolio/languages
-
   const [open, setOpen] = useState(false);
   const [languages, setLanguages] = useState([]);
   const [bannerTitle, setBannerTitle] = useState([]);
 
-  // console.log(data.name)
-
   const fetchData = async () => {
     try {
-
       //https://api.github.com/repos/soladragon/portfolio/languages
-
       //alternative https://api.github.com/search/repositories?q=user:soladragon&fork:false&direction=desc
       const response = await fetch('https://api.github.com/repos/soladragon/' + data.name + '/languages');
-
       const json = await response.json();
       await setLanguages(json);
       console.log(languages)
@@ -37,78 +27,47 @@ const PortfolioCard = ({ data, index }) => {
   }
 
   useEffect(() => {
-
-      fetchData()
-      
-
+    fetchData()
   }, []);
-
   useEffect(() => {
-
     let language = Object.keys(languages)[0];
-
-    if(language != null){
+    if (language != null) {
       language = language.replace(/[^a-zA-Z0-9 ]/g, '');
       console.log(language.toLowerCase())
       setBannerTitle(language.toLowerCase());
-
-      
-      
     }
-   
-  
-
-}, [languages]);
-
-
+  }, [languages]);
 
   useEffect(() => {
-
-    setTimeout(function () {
+    setTimeout(() => {
       // code to be executed after 3 seconds
       setOpen(true);
     }, 100 * (index + 1));
-
-
   }, []);
-
-
-  useEffect(() => {
-
-
-  }, []);
-
 
   return (
-
-
-
     <>
-
       <Fade in={open} timeout={300 * (index + 1)} >
-
-      <Card className="card shadow-sm">
-       <div className="gal-container large-4 columns">
-            <div className={`corner-ribbon top-right sticky blue shadow ${bannerTitle} ` } >{Object.keys(languages)[0]}</div>
+        <Card className="card shadow-sm">
+          <div className="gal-container large-4 columns">
+            <div className={`corner-ribbon top-right sticky blue shadow ${bannerTitle}`} >{Object.keys(languages)[0]}</div>
             {/* <img src="https://via.placeholder.com/100x75" alt="Avatar" className="gal-image" /> */}
-            <Card.Img variant="top" src="https://via.placeholder.com/250x200" className="gal-image"/>
+            <Card.Img variant="top" src="https://via.placeholder.com/250x200" className="gal-image" />
             <div className="overlay">
               <div className="text">Hello World</div>
             </div>
           </div>
-  
-      <Card.Body>
-        <Card.Title>{data.name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="outline-secondary" className="btn btn-sm">View</Button>
-        <Button variant="outline-secondary" className="btn btn-sm">Edit</Button>
-      </Card.Body>
-    </Card>
-    
 
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="outline-secondary" className="btn btn-sm">View</Button>
+            <Button variant="outline-secondary" className="btn btn-sm">Edit</Button>
+          </Card.Body>
+        </Card>
       </Fade>
     </>
   );
